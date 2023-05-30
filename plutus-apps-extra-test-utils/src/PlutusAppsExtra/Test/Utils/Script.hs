50f9ed03342096005d2f67bce150e94965068950
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-05-30 17:16:51 +0300

    Add functions for testing validators/minting policies

--- plutus-apps-extra-test-utils/src/PlutusAppsExtra/Test/Utils/Script.hs
+++ plutus-apps-extra-test-utils/src/PlutusAppsExtra/Test/Utils/Script.hs
@@ -4,24 +4,31 @@ module PlutusAppsExtra.Test.Utils.Script where
 
 import           Cardano.Api.Shelley                               (ProtocolParameters (protocolParamProtocolVersion))
 import           Cardano.Node.Emulator                             (Params (..), pProtocolParams)
-import           Control.Monad.Except                              (MonadError (..))
 import           Codec.Serialise                                   (serialise)
+import           Control.Monad.Except                              (MonadError (..))
 import           Data.Bifunctor                                    (Bifunctor (..))
+import qualified Data.ByteString.Lazy                              as LBS
+import qualified Data.ByteString.Short                             as SBS
 import           Data.Either                                       (isRight)
 import           Plutus.V1.Ledger.Api                              (CostModelApplyError (CMInternalReadError), Data,
                                                                     EvaluationContext, EvaluationError, ExBudget, LogOutput,
-                                                                    ProtocolVersion (ProtocolVersion),
-                                                                    VerboseMode (..), evaluateScriptCounting, mkEvaluationContext, Script)
+                                                                    MintingPolicy (..), ProtocolVersion (ProtocolVersion), Script,
+                                                                    Validator (..), VerboseMode (..), evaluateScriptCounting,
+                                                                    mkEvaluationContext)
 import           PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCostModelParams)
 import           Test.Hspec                                        (Expectation, expectationFailure, shouldSatisfy)
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Short as SBS
 
 testScipt :: Params -> Script -> [Data] -> Expectation
 testScipt p script plutusData = case runScriptVerbose p script plutusData of
     Left err                 -> expectationFailure $ show err
     Right (output, Left err) -> expectationFailure $ show err <> "\nOutput:\n" <> show output
     Right (_, res)           -> res `shouldSatisfy` isRight 
+
+testMintingPolicy :: Params -> MintingPolicy -> [Data] -> Expectation
+testMintingPolicy p (MintingPolicy mp) = testScipt p mp
+
+testValidator :: Params -> Validator -> [Data] -> Expectation
+testValidator p (Validator v) = testScipt p v
 
 runScript :: VerboseMode -> Params -> Script -> [Data] -> Either CostModelApplyError (LogOutput, Either EvaluationError ExBudget)
 runScript v p script plutusData = do
