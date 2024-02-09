09554f5ca9e2d7bef66b10aedeb429541961b52f
Vladislav <>
2024-02-09 14:58:15 -0300

    Lift IO for ipfs related funcs

--- plutus-apps-extra/src/PlutusAppsExtra/IO/Maestro.hs
+++ plutus-apps-extra/src/PlutusAppsExtra/IO/Maestro.hs
@@ -1,28 +1,43 @@
 {-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE OverloadedStrings   #-}
 
 module PlutusAppsExtra.IO.Maestro where
 
 import           Cardano.Api                   (NetworkId (..))
+import           Control.Monad.IO.Class        (MonadIO (..))
 import           Data.Map                      (Map)
 import qualified Data.Map                      as Map
 import           Ledger                        (PubKeyHash, TxId)
 import           Plutus.V2.Ledger.Api          (CurrencySymbol, TokenName)
 import qualified PlutusAppsExtra.Api.Maestro   as Api
-import           PlutusAppsExtra.Utils.Maestro (AccountAddressesHoldingAssetsResponse (..), Cursor, HasCursor, TxDetailsResponse,
-                                                getCursor, AssetMintsAndBurnsResponse)
+import           PlutusAppsExtra.Utils.Maestro (AccountAddressesHoldingAssetsResponse (..),
+                                                AssetMintsAndBurnsResponse,
+                                                Cursor, HasCursor,
+                                                TxDetailsResponse, getCursor)
 import           PlutusAppsExtra.Utils.Servant (handle404Maybe)
 
-getAccountAddressesHoldingAssets :: NetworkId -> CurrencySymbol -> TokenName -> IO (Map PubKeyHash Integer)
-getAccountAddressesHoldingAssets network cs name = fmap (Map.fromList . concatMap (\AccountAddressesHoldingAssetsResponse{..} -> aaharData))
-    $ foldPages $ Api.getAccountAddressesHoldingAssets network cs name
+getAccountAddressesHoldingAssets :: MonadIO m
+  => NetworkId
+  -> CurrencySymbol
+  -> TokenName
+  -> m (Map PubKeyHash Integer)
+getAccountAddressesHoldingAssets network cs name =
+  fmap (Map.fromList . concatMap (\AccountAddressesHoldingAssetsResponse{..} -> aaharData))
+    $ liftIO $ foldPages $ Api.getAccountAddressesHoldingAssets network cs name
 
-getAssetMintsAndBurns :: NetworkId -> CurrencySymbol -> TokenName -> IO [AssetMintsAndBurnsResponse]
-getAssetMintsAndBurns network cs name = foldPages $ Api.getAssetMintsAndBurns network cs name
+getAssetMintsAndBurns :: MonadIO m
+  => NetworkId
+  -> CurrencySymbol
+  -> TokenName
+  -> m [AssetMintsAndBurnsResponse]
+getAssetMintsAndBurns network cs name = liftIO $ foldPages $ Api.getAssetMintsAndBurns network cs name
 
-getTxDetails :: NetworkId -> TxId -> IO (Maybe TxDetailsResponse)
-getTxDetails network = handle404Maybe . Api.getTxDetails network
+getTxDetails :: MonadIO m
+  => NetworkId
+  -> TxId
+  -> m (Maybe TxDetailsResponse)
+getTxDetails network = liftIO . handle404Maybe . Api.getTxDetails network
 
 foldPages :: HasCursor a => (Maybe Cursor -> IO a) -> IO [a]
 foldPages f = go $ f Nothing
@@ -31,4 +46,4 @@ foldPages f = go $ f Nothing
             res <- getRes
             case getCursor res of
                 Nothing -> pure [res]
-                Just s -> (res :) <$> go (f $ Just s)
+                Just s  -> (res :) <$> go (f $ Just s)
