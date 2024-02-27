3129db6a7932c824b920d858dc8afdcd9e71985a
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-02-27 16:59:52 +0300

    Add HasTxProvider class

---
+++ plutus-apps-extra/src/PlutusAppsExtra/IO/Tx.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE EmptyDataDeriving     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NumericUnderscores    #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module PlutusAppsExtra.IO.Tx where
+
+import           Cardano.Node.Emulator            (Params)
+import           Control.Monad.IO.Class           (MonadIO (..))
+import qualified Data.Map                         as Map
+import           Ledger                           (CardanoTx (..), DecoratedTxOut (..), PaymentPubKeyHash (PaymentPubKeyHash), PubKeyHash,
+                                                   StakingCredential, TxOutRef, _decoratedTxOutAddress, fromCardanoValue,
+                                                   getCardanoTxOutputs, toPlutusAddress, txOutAddress, txOutValue)
+import           Ledger.Tx.CardanoAPI             (unspentOutputsTx)
+import           Ledger.Tx.Constraints            (ScriptLookups, TxConstraints, mustPayToPubKey, mustPayToPubKeyAddress)
+import           Ledger.Typed.Scripts             (Any, ValidatorTypes (..))
+import qualified Plutus.Script.Utils.Ada          as Ada
+import           Plutus.Script.Utils.Value        (leq)
+import qualified Plutus.V2.Ledger.Api             as P
+import           PlutusAppsExtra.IO.ChainIndex    (HasChainIndex)
+import           PlutusAppsExtra.IO.Wallet        (HasWalletProvider (..))
+import           PlutusAppsExtra.Utils.ChainIndex (MapUTXO)
+import           PlutusTx.IsData                  (FromData, ToData)
+import           PlutusTx.Prelude                 (zero, (-))
+import           Prelude                          hiding ((-))
+
+data TxPrvider
+    deriving (Show, Eq)
+
+class (HasWalletProvider m, HasChainIndex m) => HasTxProvider m where
+
+    getTxProvider :: m TxPrvider
+
+    signTx :: CardanoTx -> m CardanoTx
+
+    balanceTx :: (FromData (DatumType Any), ToData (DatumType Any), ToData (RedeemerType Any), Show (DatumType Any),
+        Show (RedeemerType Any)) => Params -> ScriptLookups Any -> TxConstraints (RedeemerType Any) (DatumType Any) -> m CardanoTx
+
+    submitTx :: CardanoTx -> m ()
+
+    awaitTxConfirmed :: CardanoTx -> m ()
+
+-- Send a balanced transaction to Cardano Wallet Backend and wait until transaction is confirmed or declined
+submitTxConfirmed :: HasTxProvider m => CardanoTx -> m ()
+submitTxConfirmed ctx = submitTx ctx >> awaitTxConfirmed ctx
+
+-- Get wallet total profit from a transaction.
+getTxProfit :: HasWalletProvider m => CardanoTx -> MapUTXO -> m P.Value
+getTxProfit tx txUtxos = do
+        addrs <- getWalletAddresses
+        let txOuts   = Map.elems txUtxos
+            spent    = getTotalValue addrs _decoratedTxOutValue _decoratedTxOutAddress txOuts
+            produced = getTotalValue addrs txOutValue (toPlutusAddress . txOutAddress) $ getCardanoTxOutputs tx
+        pure $ fromCardanoValue produced - fromCardanoValue spent
+    where
+        getTotalValue addrs getValue getAddr = mconcat . map getValue . filter ((`elem` addrs) . getAddr)
+
+isProfitableTx :: HasWalletProvider m => CardanoTx -> MapUTXO -> m Bool
+isProfitableTx tx txUtxos = leq zero <$> getTxProfit tx txUtxos
+
+-- Create and submit a transaction that produces a specific number of outputs at the target wallet address
+getWalletTxOutRefs :: HasTxProvider m => Params -> PubKeyHash -> Maybe StakingCredential -> Int -> m [TxOutRef]
+getWalletTxOutRefs params pkh mbSkc n = do
+    liftIO $ putStrLn "Balancing..."
+    balancedTx <- balanceTx params lookups cons
+    liftIO $ print balancedTx
+    liftIO $ putStrLn "Signing..."
+    signedTx <- signTx balancedTx
+    liftIO $ print signedTx
+    liftIO $ putStrLn "Submitting..."
+    submitTxConfirmed signedTx
+    let refs = Map.keys $ unspentOutputsTx signedTx
+    liftIO $ putStrLn "Submitted!"
+    return refs
+    where
+        lookups = mempty :: ScriptLookups Any
+        cons    = case mbSkc of
+            Just skc -> mconcat $ replicate n $ mustPayToPubKeyAddress (PaymentPubKeyHash pkh) skc $ Ada.lovelaceValueOf 10_000_000
+            Nothing -> mustPayToPubKey (PaymentPubKeyHash pkh) $ Ada.lovelaceValueOf 10_000_000
