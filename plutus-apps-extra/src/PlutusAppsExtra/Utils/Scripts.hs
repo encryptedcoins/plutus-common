c5ad67ae9ff3288ce1a88b90d76c31c79e670b05
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-06-10 16:11:02 +0300

    Add script utils

--- plutus-apps-extra/src/PlutusAppsExtra/Utils/Scripts.hs
+++ plutus-apps-extra/src/PlutusAppsExtra/Utils/Scripts.hs
@@ -1,34 +1,22 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 
 module PlutusAppsExtra.Utils.Scripts where
 
--- import           Codec.Serialise      (deserialise, serialise)
--- import qualified Data.ByteString.Lazy as LBS
--- import           Data.Coerce          (Coercible, coerce)
--- import           Data.Maybe           (fromJust)
--- import           Ledger               (MintingPolicy (..), Script (..), Validator (..))
--- import           PlutusCore           (DefaultFun, DefaultUni)
--- import           Text.Hex             (Text, decodeHex, encodeHex)
--- import           UntypedPlutusCore    (DeBruijn, Program (..))
-import qualified Codec.CBOR.Decoding       as CBOR
-import           Codec.Serialise           (Serialise (encode), decode, deserialise, serialise)
-import           Control.FromSum           (eitherToMaybe)
-import           Control.Monad.Error.Class (MonadError)
-import qualified Data.ByteString.Lazy      as LBS
-import           Data.ByteString.Short     (ShortByteString)
-import qualified Data.ByteString.Short     as SBS
-import           Data.Coerce               (Coercible, coerce)
-import           Data.Maybe                (fromJust)
-import qualified Flat
-import qualified Flat.Decoder              as Flat
-import           Ledger                    (MintingPolicy (..), Script (..), Validator (..))
-import           PlutusCore                (DefaultFun, DefaultUni, latestVersion)
-import           PlutusCore.Error          (ApplyProgramError)
-import           PlutusCore.MkPlc          (mkConstant)
-import           PlutusLedgerApi.V3        (ToData, serialiseUPLC, toData, uncheckedDeserialiseUPLC)
-import           Text.Hex                  (Text, decodeHex, encodeHex)
-import           UntypedPlutusCore         (DeBruijn, Program (..), applyProgram)
+import           Codec.Serialise       (deserialise, serialise)
+import           Control.FromSum       (eitherToMaybe)
+import qualified Data.ByteString.Lazy  as LBS
+import qualified Data.ByteString.Short as SBS
+import           Data.Coerce           (Coercible, coerce)
+import           Data.Maybe            (fromJust)
+import           Ledger                (MintingPolicy (..), Script (..), Validator (..))
+import           PlutusCore            (DefaultFun, DefaultUni, latestVersion)
+import           PlutusCore.MkPlc      (mkConstant)
+import           PlutusLedgerApi.V3    (SerialisedScript, ToData, serialiseUPLC, toData, uncheckedDeserialiseUPLC)
+import           Text.Hex              (Text, decodeHex, encodeHex)
+import           UntypedPlutusCore     (DeBruijn, Program (..), applyProgram)
 
 type UPLCProgram = Program DeBruijn DefaultUni DefaultFun ()
 
@@ -62,24 +50,23 @@ unsafeMintingPolicyFromCBOR = fromJust . mintingPolicyFromCBOR
 
 --------------- Parameterized from CBOR ------------------
 
--- TODO: figure out how to do this in new plutus.
+parameterizedScriptFromCBOR :: (ToData par, Coercible SerialisedScript script)
+    => Text -> par -> Maybe script
+parameterizedScriptFromCBOR txt par = do
+    bs <- decodeHex txt
+    let programFun = uncheckedDeserialiseUPLC $ SBS.toShort bs
+        programPar = Program () latestVersion $ mkConstant () (toData par)
+    program <- eitherToMaybe $ programFun `applyProgram` programPar
+    pure $ coerce $ serialiseUPLC program
 
--- parameterizedScriptFromCBOR :: (ToData par, Coercible UPLCProgram script)
---     => Text -> par -> Maybe script
--- parameterizedScriptFromCBOR txt par = do
---     bs <- decodeHex txt
---     let program = uncheckedDeserialiseUPLC $ SBS.toShort bs
---         p = Program () latestVersion . mkConstant ()
---     eitherToMaybe $ coerce <$> program `applyProgram` p (toData par)
+parameterizedValidatorFromCBOR :: forall par. (ToData par) => Text -> par -> Maybe Validator
+parameterizedValidatorFromCBOR = parameterizedScriptFromCBOR
 
--- parameterizedValidatorFromCBOR :: (ToData par) => Text -> par -> Maybe Validator
--- parameterizedValidatorFromCBOR = parameterizedScriptFromCBOR
+unsafeParameterizedValidatorFromCBOR :: (ToData par) => Text -> par -> Validator
+unsafeParameterizedValidatorFromCBOR txt = fromJust . parameterizedValidatorFromCBOR txt
 
--- unsafeParameterizedValidatorFromCBOR :: (ToData par) => Text -> par -> Validator
--- unsafeParameterizedValidatorFromCBOR txt = fromJust . parameterizedValidatorFromCBOR txt
+parameterizedMintingPolicyFromCBOR :: (ToData par) => Text -> par -> Maybe MintingPolicy
+parameterizedMintingPolicyFromCBOR = parameterizedScriptFromCBOR
 
--- parameterizedMintingPolicyFromCBOR :: (ToData par) => Text -> par -> Maybe MintingPolicy
--- parameterizedMintingPolicyFromCBOR txt = fromJust . parameterizedScriptFromCBOR txt
-
--- unsafeParameterizedMintingPolicyFromCBOR :: (ToData a) => Text -> a -> MintingPolicy
--- unsafeParameterizedMintingPolicyFromCBOR = fromJust . parameterizedMintingPolicyFromCBOR
+unsafeParameterizedMintingPolicyFromCBOR :: (ToData a) => Text -> a -> MintingPolicy
+unsafeParameterizedMintingPolicyFromCBOR txt par = fromJust $ parameterizedMintingPolicyFromCBOR txt par
